8e5a8c13545b0b42a56ace64ced1d3565c00c3aa
jonfry1175 <odiboen@gmail.com>
2024-08-17 01:34:23 +0700

    setup express

---
+++ express/app.js
@@ -0,0 +1,17 @@
+const express = require('express'); 
+const cors = require('cors');
+const app = express();
+const port = 3000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+
+
+// route
+app.get('/', (req, res) => res.send('Hello World!'));
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}!`)
+})
