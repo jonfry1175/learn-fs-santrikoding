e04c01295b0f542f2b7218390c59c3c4fca08911
jonfry1175 <odiboen@gmail.com>
2024-08-19 22:16:28 +0700

    gitignore

--- express/app.js
+++ express/app.js
@@ -11,8 +11,6 @@ app.use(cors());
 
 app.use('/api', routes);
 
-
-
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}!`)
+    console.log(`Example app listening on http://localhost:${port}!`)
 })
